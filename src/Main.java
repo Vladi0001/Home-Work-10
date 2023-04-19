74875a70e34d53243775718076fa0d9ca7675029
Vladi0404 <moskva.vladi@yandex.ru>
2023-04-19 09:16:56 +0200

    Finished homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // task 1
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+
+        // task 2
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + lastName.charAt(0) + "." + firstName.charAt(0) + "." + middleName.charAt(0) + ".");
+
+        //task 3
+
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+
+        System.out.println(fullName);
+
+
     }
 }
